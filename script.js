0eb8ddfd0b4544e93020327143847e82786829fd
KANIKAMA2434 <113009392+KANIKAMA2434@users.noreply.github.com>
2026-07-25 09:03:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,104 @@
+// =====================
+// スクロール表示
+// =====================
 
+
+const cards = document.querySelectorAll(".card");
+
+
+function showCards(){
+
+    cards.forEach(card => {
+
+        const position =
+        card.getBoundingClientRect().top;
+
+
+        const screen =
+        window.innerHeight;
+
+
+        if(position < screen - 100){
+
+            card.classList.add("show");
+
+        }
+
+    });
+
+}
+
+
+
+window.addEventListener(
+"scroll",
+showCards
+);
+
+
+
+showCards();
+
+
+
+
+
+
+// =====================
+// TOPボタン
+// =====================
+
+
+const topButton =
+document.getElementById("top");
+
+
+
+topButton.addEventListener(
+"click",
+()=>{
+
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+
+});
+
+
+
+
+
+
+// =====================
+// ページ読み込み演出
+// =====================
+
+
+window.addEventListener(
+"load",
+()=>{
+
+
+document.body.style.opacity="0";
+
+
+setTimeout(()=>{
+
+
+document.body.style.transition="1s";
+
+
+document.body.style.opacity="1";
+
+
+},100);
+
+
+
+});
